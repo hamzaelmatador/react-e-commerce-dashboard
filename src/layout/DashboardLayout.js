6f94d9e5f284aa0fda5ccd86a45067ba1cbd3037
hamza <hamnaddari@gmail.com>
2026-01-04 04:18:29 +0100

    make sidebar collapsible

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -1,12 +1,23 @@
 import Sidebar from './Sidebar';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
+
 import '../styles/dashboardLayout.css';
 
 function DashboardLayout({ children }) {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="dashboard-layout">
-      <Sidebar />
-      <div className="dashboard-content">
+      <Sidebar isOpen={sidebarOpen} closeSidebar={() => setSidebarOpen(false)}/>
+      <div className="dashboard-content"  >
+        <button className="menu-btn" onClick={() => {setSidebarOpen(!sidebarOpen)}}>
+           <FontAwesomeIcon icon={faBars} />
+        </button>
         {children}
+        <h1>Hello</h1>
       </div>
     </div>
   );
--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -12,46 +12,46 @@ import {
   faCannabis,
 } from "@fortawesome/free-solid-svg-icons";
 
-function Sidebar() {
+function Sidebar({isOpen , closeSidebar}) {
   return (
-    <aside className="sidebar">
+    <aside className={`sidebar ${isOpen ? "open" : ""} `}>
       <div className="sidebarheader">
         <FontAwesomeIcon icon={faCannabis} className="logo-icon" />
         <h2>Cris<span>Weed</span></h2>
       </div>
 
       <nav>
-        <NavLink to="/" end>
+        <NavLink to="/" end onClick={closeSidebar} >
           <FontAwesomeIcon icon={faTachometerAlt} />
           <p>Dashboard</p>
         </NavLink>
 
-        <NavLink to="/products">
+        <NavLink to="/products" onClick={closeSidebar}>
           <FontAwesomeIcon icon={faShoppingBag} />
           <p>Products</p>
         </NavLink>
 
-        <NavLink to="/orders">
+        <NavLink to="/orders" onClick={closeSidebar}>
           <FontAwesomeIcon icon={faShoppingCart} />
           <p>Orders</p>
         </NavLink>
 
-        <NavLink to="/customers">
+        <NavLink to="/customers" onClick={closeSidebar}>
           <FontAwesomeIcon icon={faUsers} />
           <p>Customers</p>
         </NavLink>
 
-        <NavLink to="/analytics">
+        <NavLink to="/analytics" onClick={closeSidebar} >
           <FontAwesomeIcon icon={faChartBar} />
           <p>Analytics</p>
         </NavLink>
 
-        <NavLink to="/categories">
+        <NavLink to="/categories" onClick={closeSidebar} >
           <FontAwesomeIcon icon={faTags} />
           <p>Categories</p>
         </NavLink>
 
-        <NavLink to="/settings">
+        <NavLink to="/settings" onClick={closeSidebar}>
           <FontAwesomeIcon icon={faCog} />
           <p>Settings</p>
         </NavLink>
